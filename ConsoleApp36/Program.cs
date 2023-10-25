4887d259387bcc01a57e9a065dc6255ad4573373
kvtsuki <117033172+kvtsuki@users.noreply.github.com>
2023-10-25 12:02:08 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp36/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp36
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //ВСЕ РАЗЛИЧНЫЕ ЦИФРЫ
+            //string str = Console.ReadLine();
+            //string result = String.Empty;
+            //foreach (char item in str)
+            //{
+            //    if (char.IsDigit(item))
+            //    {
+            //        if (!result.Contains(item))
+            //        {
+            //            result += item;
+            //        }
+            //    }
+            //}
+            //if (result == String.Empty)
+            //{
+            //    result = "NO";
+            //}
+            //Console.WriteLine(result);
+
+
+
+
+            //ПОВТОРЯЮЩИЕСЯ ЦИФРЫ
+            //string str = Console.ReadLine();
+            //string number = string.Empty;
+            //string result = string.Empty;
+            //foreach (char item in str)
+            //{
+            //    if (!result.Contains(item))
+            //    {
+            //        result += item;
+            //    }
+            //}
+            //foreach (char item in result)
+            //{
+            //    int count = 0;
+            //    foreach (char symbol in str)
+            //    {
+            //        if (item == symbol)
+            //        {
+            //            count++;
+            //        }
+            //    }
+            //    if (count > 1 && Char.IsDigit(item))
+            //    {
+            //        number += item;
+            //    }
+
+            //}
+            //if (number == string.Empty)
+            //{
+            //    number = "NO";
+            //}
+
+            //Console.WriteLine(number);
+
+
+
+
+
+
+            //ПРАВИЛЬНОЕ ИМЯ ПЕРЕМЕННОЙ
+            //string str = Console.ReadLine();
+            //string pattern = "abcdefghijklmnopqrstuvwxyz0123456789_";
+            //str = str.ToLower();
+            //if (Char.IsDigit(str.First()))
+            //{
+            //    Console.WriteLine("NO");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("YES");
+            //}
+
+
+
+
+
+
+            //СКОЛЬКО РАЗЛИЧНЫХ СИМВОЛОВ
+            //string str = Console.ReadLine().ToLower();
+            //string pattern = "abcdefghijklmnopqrstuvwxyz0123456789_";
+            //string result = string.Empty;
+            //foreach (char item in str)
+            //{
+            //    if (Char.IsDigit(item))
+            //    {
+            //        if (!result.Contains(item))
+            //        {
+            //            result += item;
+            //        }
+            //    }
+
+            //}
+            //foreach (char item in result)
+            //{
+
+            //    pattern = pattern.Replace(item.ToString(), string.Empty);
+
+            //}
+            //if (pattern == string.Empty)
+            //{ 
+            //    pattern = "NO";
+            //}
+            //Console.WriteLine(result);
+
+
+
+
+
+
+
+
+
+            //ЦИФРЫ, КОТОРЫХ НЕТ
+            //string str = Console.ReadLine().ToLower();
+            //string str2 = Console.ReadLine().ToLower();
+            //string xy = str + str2;
+            //string pattern = "abcdefghijklmnopqrstuvwxyz";
+            //string result = string.Empty;
+            //foreach (char item in xy)
+            //{
+            //    if (Char.IsLetter(item))
+            //    {
+            //        if (!result.Contains(item))
+            //        {
+            //            result += item;
+            //        }
+            //    }
+            //}
+            //foreach (char item in result)
+            //{
+            //    pattern = pattern.Replace(item.ToString(), string.Empty);
+            //}
+            //if (pattern == string.Empty)
+            //{
+            //    pattern = "()";
+            //}
+
+            //Console.WriteLine(pattern.ToUpper());
+
+
+
+
+
+
+
+
+
+           
+        }
+    }
+}
